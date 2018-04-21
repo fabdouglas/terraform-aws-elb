421067c3c8b5eacafabb0c326305b79b4332b3c0
Fabrice Daugan <fabrice.daugan@gmail.com>
2018-04-21 09:56:12 +0200

    Fiw warning about count

--- modules/elb/outputs.tf
+++ modules/elb/outputs.tf
@@ -21,7 +21,7 @@ output "this_elb_dns_name" {
 
 output "this_elb_instances" {
   description = "The list of instances in the ELB"
-  value       = ["${compact(split(",", var.create_elb ? join(",", aws_elb.this.*.instances) : ""))}"]
+  value       = ["${flatten(aws_elb.this.*.instances)}"]
 }
 
 output "this_elb_source_security_group_id" {
